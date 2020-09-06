86b1ca06e5228a68b0e4c3f25c73a18c3189db30
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-06 21:27:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf|
+    puts "#{index.to_i+1} #{dwarf} "
 end
 
 def summon_captain_planet# code an argument here
